3e6b0bf0f1e44becedc9bd1a4674912412ee0669
ali-hajeb <ali.hajeb@hotmail.com>
2025-07-19 22:10:47 +0330

    Migrated to seperate repositories

--- lz77/include/constants.h
+++
@@ -1,8 +0,0 @@
-
-#ifndef CONSTANTS_H
-#define CONSTANTS_H
-#define KB 1024
-#define COMPRESSED_BUFFER_SIZE (2 * KB)
-#define DECOMPRESSED_BUFFER_SIZE (4 * KB)
-#define WINDOW_SIZE (16 * KB)
-#endif
--- rle/include/constants.h
+++
@@ -1,6 +0,0 @@
-#ifndef CONSTANTS_H
-#define CONSTANTS_H
-#define KB 1024
-#define COMPRESSED_BUFFER_SIZE (2 * KB)
-#define DECOMPRESSED_BUFFER_SIZE (4 * KB)
-#endif
